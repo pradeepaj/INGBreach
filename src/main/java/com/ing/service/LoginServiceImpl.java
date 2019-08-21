91e7cf39aa503337d2676f4f6dd5314329ddb8fc
User1 <User1@LP-5CD9176LCF.HCLT.CORP.HCL.IN>
2019-08-21 12:15:47 +0530

    login done

--- src/main/java/com/ing/service/LoginServiceImpl.java
+++ src/main/java/com/ing/service/LoginServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import com.ing.dto.LoginReqDto;
 import com.ing.dto.LoginResDto;
 import com.ing.entity.UserDetails;
+import com.ing.exception.EnterValidCredentials;
+import com.ing.exception.UserNotFound;
 import com.ing.repository.UserRepository;
 @Service
 public class LoginServiceImpl implements LoginService {
@@ -15,12 +17,29 @@ public class LoginServiceImpl implements LoginService {
 	
 	@Override
 	public LoginResDto userLogin(LoginReqDto loginDto) {
-		UserDetails details=userRepository.findByEmailAndPassword(loginDto.getEmail(),loginDto.getPassword());
-		LoginResDto loginResDto=new LoginResDto();
-		loginResDto.setUserId(details.getUserId());
-		loginResDto.getRole(details.getRole());
 		
-		return null;
+		
+		UserDetails details=userRepository.findByEmailAndPassword(loginDto.getEmail(),loginDto.getPassword());
+		
+		if(null!=details) {
+			if (details.getEmail().equals(loginDto.getEmail()) && details.getPassword().equals(loginDto.getPassword())) {
+				LoginResDto loginResDto=new LoginResDto();
+				loginResDto.setUserId(details.getUserId());
+				loginResDto.setRole(details.getRole());
+				loginResDto.setUserName(details.getUserName());
+				loginResDto.setMessage("Login Success");
+				return loginResDto;
+			}else {
+				throw new EnterValidCredentials("Please enter correct username or password");
+			}
+			
+		}
+		else {
+			throw new UserNotFound("User does not exists");
+		}
+		
+		
+		
 	}
 
 }
